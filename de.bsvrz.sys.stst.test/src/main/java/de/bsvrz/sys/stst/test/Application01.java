67fff7ef34acff8b88de2e2e0436e95fea876ace
Uwe Peuker <dev@upeuker.net>
2017-07-22 18:04:12 +0200

    Prozeß in einzelne Maven/Gradle-Module zerlegt, SWE für Testapplikationen ergänzt, Dokumentationserstellung vereinfacht

---
+++ de.bsvrz.sys.stst.test/src/main/java/de/bsvrz/sys/stst/test/Application01.java
@@ -0,0 +1,20 @@
+package de.bsvrz.sys.stst.test;
+
+import de.bsvrz.dav.daf.main.ClientDavInterface;
+import de.bsvrz.sys.funclib.application.StandardApplication;
+import de.bsvrz.sys.funclib.commandLineArgs.ArgumentList;
+
+public class Application01 implements StandardApplication {
+
+	@Override
+	public void parseArguments(ArgumentList argumentList) throws Exception {
+		// keine Extra-Argumente erwartet
+		
+	}
+
+	@Override
+	public void initialize(ClientDavInterface connection) throws Exception {
+		// die Applikation tut nichts
+	}
+
+}
