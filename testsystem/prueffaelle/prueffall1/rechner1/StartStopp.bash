252421c6029eb0ea105e455d9f92ff7c639e9a69
ChristianHoesel <christianhoesel79@gmail.com>
2017-08-31 13:32:32 +0200

    Prüffall 1: Bash Skripte Rechner 1

---
+++ testsystem/prueffaelle/prueffall1/rechner1/StartStopp.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+. einstellungen.sh
+
+# StartStopp
+$java \
+  -cp ../../../distributionspakete/de.bsvrz.sys.startstopp/de.bsvrz.sys.startstopp-runtime.jar \
+ de.bsvrz.sys.startstopp.startstopp.StartStopp \
+ -benutzerKonfiguration=../../../konfiguration/benutzerverwaltung.xml \
+ -authentifizierung=passwd \
+ -betriebsMeldungVersenden=nein \
+ &
+
+# Auf das Ende von allen im Hintergrund gestarteten Prozessen warten
+wait
---
+++ testsystem/prueffaelle/prueffall1/rechner1/StartStoppGUI.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+. einstellungen.sh
+
+# StartStopp
+$java \
+   -cp ../../../distributionspakete/de.bsvrz.sys.startstopp/de.bsvrz.sys.startstopp-runtime.jar \
+ de.bsvrz.sys.startstopp.console.StartStoppConsole &
+
+# Auf das Ende von allen im Hintergrund gestarteten Prozessen warten
+wait
