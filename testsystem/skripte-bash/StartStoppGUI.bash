058379d70575d12f8eaa6e72505973aa72d0192d
ChristianHoesel <christianhoesel79@gmail.com>
2017-08-31 14:14:22 +0200

    Prüffälle nicht in einzelne Unterverzeichnisse verteilen

--- testsystem/skripte-bash/StartStoppGUI.bash
+++ testsystem/skripte-bash/StartStoppGUI.bash
@@ -3,7 +3,7 @@
 
 # StartStopp
 $java \
-   -cp ../../../distributionspakete/de.bsvrz.sys.startstopp/de.bsvrz.sys.startstopp-runtime.jar \
+   -cp ../distributionspakete/de.bsvrz.sys.startstopp/de.bsvrz.sys.startstopp-runtime.jar \
  de.bsvrz.sys.startstopp.console.StartStoppConsole &
 
 # Auf das Ende von allen im Hintergrund gestarteten Prozessen warten
