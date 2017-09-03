8aa186d3e7f8cfba8bdc78e47a05eb8fec955c4c
Uwe Peuker <dev@upeuker.net>
2017-09-03 11:55:45 +0200

    Applikation02 beendet sich, weil sie zyklisch ausgeführt werden soll

--- de.bsvrz.sys.stst.test/src/main/java/de/bsvrz/sys/stst/test/Application02.java
+++ de.bsvrz.sys.stst.test/src/main/java/de/bsvrz/sys/stst/test/Application02.java
@@ -1,5 +1,9 @@
 package de.bsvrz.sys.stst.test;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 import de.bsvrz.dav.daf.main.ClientDavInterface;
 import de.bsvrz.sys.funclib.application.StandardApplication;
 import de.bsvrz.sys.funclib.application.StandardApplicationRunner;
@@ -15,7 +19,8 @@ public class Application02 implements StandardApplication {
 
 	@Override
 	public void initialize(ClientDavInterface connection) throws Exception {
-		// die Applikation tut nichts
+		// die Applikation beendet sich nach 30 Sekunden
+		Executors.newSingleThreadScheduledExecutor().schedule(()->System.exit(0), 30, TimeUnit.SECONDS);
 	}
 	
 	public static void main(String[] args) {
