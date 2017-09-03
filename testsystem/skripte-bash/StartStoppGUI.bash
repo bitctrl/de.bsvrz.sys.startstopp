8b2cf0679ec7c6140fa7cb08233081aa6b8f4c2b
Uwe Peuker <dev@upeuker.net>
2017-09-03 11:15:40 +0200

    Lanterna-Natives entfernt StartSkript für GUI im Testsystem korrigiert

--- testsystem/skripte-bash/StartStoppGUI.bash
+++ testsystem/skripte-bash/StartStoppGUI.bash
@@ -4,7 +4,7 @@
 # StartStopp
 $java \
    -cp ../distributionspakete/de.bsvrz.sys.startstopp/de.bsvrz.sys.startstopp-runtime.jar \
- de.bsvrz.sys.startstopp.console.StartStoppConsole &
+ de.bsvrz.sys.startstopp.console.StartStoppConsole
 
 # Auf das Ende von allen im Hintergrund gestarteten Prozessen warten
 wait
